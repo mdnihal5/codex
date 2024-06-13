ea4fd4c9922dac59c104774a8c6d00ec029c5e87
mdnihal5 <mdnihal.private1@gmail.com>
2024-06-13 13:12:31 +0530

    chat added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
         return () => {
           clearInterval(intervalId);
         };
-    }, []);
+    }, [isAuthenticated]);
     return (
         <>
             <Routes>
